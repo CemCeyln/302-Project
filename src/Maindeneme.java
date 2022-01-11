0c4cd63bc0145a05ac7c17151b8c138b92a7d04e
zeynep <zeynephclar97@gmail.com>
2022-01-12 02:20:56 +0300

    MainDeneme class content loaded.

--- src/Maindeneme.java
+++ src/Maindeneme.java
@@ -1,3 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Maindeneme {
+    public static void main (String []args) {
+        JFrame frame = new JFrame(),
+                frame.setSize(1000,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLayout(new FlowLayout());
+
+    }
+
 
 }
