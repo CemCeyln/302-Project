cc1bf1080ff7ef24288cf08d68000c8045a03da2
Cem <cem.cey@outlook.com>
2022-01-11 23:15:40 +0300

    Creating sub-trees is completed.

--- src/InputGetDeneme.java
+++ src/InputGetDeneme.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 public class InputGetDeneme {
     public static int personID = 1;
     public static HashMap<Integer, DefaultMutableTreeNode> familyTree = new HashMap<Integer, DefaultMutableTreeNode>();
+    public static ArrayList<Relation> Relations = new ArrayList<Relation>();
+    public static JTree tree;
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(1000,500);
@@ -37,32 +39,78 @@ public class InputGetDeneme {
                 newFrame.setSize(1000,500);
                 JPanel leftPanel = new JPanel();
                 JPanel rightPanel = new JPanel();
+                //Sol üst taraf
+                JLabel labelExp = new JLabel("Please use this for adding child in the tree");
+                labelExp.setPreferredSize(new Dimension(15,15));
+                JLabel labelExp2 = new JLabel("If you are adding first person to the tree use ID as 0");
+                labelExp2.setPreferredSize(new Dimension(15,15));
                 JLabel labelParent = new JLabel("ParentID");
-                JTextField getParentID = new JTextField(20);
+                JTextField getParentID = new JTextField(15);
                 JLabel labelName = new JLabel("Name");
-                JTextField getName = new JTextField(20);
+                JTextField getName = new JTextField(15);
                 JLabel labelBirthdate = new JLabel("Birthdate");
-                JTextField getbirthDate = new JTextField(20);
+                JTextField getbirthDate = new JTextField(15);
                 JLabel labelGender = new JLabel("gender");
-                JTextField getGender = new JTextField(20);
-                JButton btn = new JButton("Add");
+                JTextField getGender = new JTextField(15);
+                JButton btn = new JButton("Add Child");
 
+                //Sağ üst taraf
+                JLabel labelExp3 = new JLabel("Please use this for adding spouse in the tree");
+                labelExp3.setPreferredSize(new Dimension(15,15));
+                JLabel labelExp4 = new JLabel("If you are adding first person to the tree use ID as 0");
+                labelExp4.setPreferredSize(new Dimension(15,15));
+                JLabel labelSpouseID = new JLabel("SpouseID");
+                JTextField getSpouseID = new JTextField(15);
+                JLabel labelNameSpouse = new JLabel("Name");
+                JTextField getNameSpouse = new JTextField(15);
+                JLabel labelBirthdateSpouse = new JLabel("Birthdate");
+                JTextField getbirthDateSpouse = new JTextField(15);
+                JLabel labelGenderSpouse = new JLabel("gender");
+                JTextField getGenderSpouse = new JTextField(15);
+                JButton btn2 = new JButton("Add Spouse");
 
-                rightPanel.add(labelParent);
-                rightPanel.add(getParentID);
-                rightPanel.add(labelName);
-                rightPanel.add(getName);
-                rightPanel.add(labelBirthdate);
-                rightPanel.add(getbirthDate);
-                rightPanel.add(labelGender);
-                rightPanel.add(getGender);
-                rightPanel.add(btn);
+                rightPanel.setLayout(new GridLayout(2,2));
+                JPanel topLeft = new JPanel();
+                topLeft.setLayout(new BoxLayout(topLeft, BoxLayout.PAGE_AXIS));
+                JPanel topRight = new JPanel();
+                topRight.setLayout(new BoxLayout(topRight,BoxLayout.PAGE_AXIS));
+                JPanel botLeft = new JPanel();
+                JPanel botRight = new JPanel();
+
+                //Sol panele ekleme
+                topLeft.add(labelExp);
+                topLeft.add(labelExp2);
+                topLeft.add(labelParent);
+                topLeft.add(getParentID);
+                topLeft.add(labelName);
+                topLeft.add(getName);
+                topLeft.add(labelBirthdate);
+                topLeft.add(getbirthDate);
+                topLeft.add(labelGender);
+                topLeft.add(getGender);
+                topLeft.add(btn);
+
+                //Sağ panele ekleme
+                topRight.add(labelExp3);
+                topRight.add(labelExp4);
+                topRight.add(labelSpouseID);
+                topRight.add(getSpouseID);
+                topRight.add(labelNameSpouse);
+                topRight.add(getNameSpouse);
+                topRight.add(labelBirthdateSpouse);
+                topRight.add(getbirthDateSpouse);
+                topRight.add(labelGenderSpouse);
+                topRight.add(getGenderSpouse);
+                topRight.add(btn2);
+
+                rightPanel.add(topLeft);
+                rightPanel.add(topRight);
+                rightPanel.add(botLeft);
+                rightPanel.add(botRight);
 
                 JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(leftPanel),new JScrollPane(rightPanel));
                 mainSplitPane.setResizeWeight(0.4);
-                //JSplitPane leftSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JScrollPane(leftTopPanel),new JScrollPane(leftBotPanel));
-                //rightPanel.add(leftSplitPane);
-                //rightPanel.add(leftTopPanel,leftBotPanel);
+                //Add child button
                 btn.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -78,25 +126,59 @@ public class InputGetDeneme {
                             newPerson.setGender(gender);
                             newPerson.setBirthDate(birthdate);
                             DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(newPerson.getName() + " - " + newPerson.getID() + " - " + newPerson.getBirthDate() + " - " + newPerson.getGender());
-                            JTree tree = new JTree(rootNode);
+                            tree = new JTree(rootNode);
                             familyTree.put(personID,rootNode);
                             leftPanel.add(tree);
-                            newFrame.pack();
                         }
                         else
                         {
                             DefaultMutableTreeNode parentNode = familyTree.get(parentID);
-                            //parentPerson = peopleInTree.get(parentID);
-                            DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(newPerson.getName() + " - " + newPerson.getID() + " - " + newPerson.getBirthDate() + " - " + newPerson.getGender());
-                            newPerson.setID(personID++);
+                            newPerson.setID(++personID);
                             newPerson.setName(name);
                             newPerson.setGender(gender);
                             newPerson.setBirthDate(birthdate);
+                            DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(newPerson.getName() + " - " + newPerson.getID() + " - " + newPerson.getBirthDate() + " - " + newPerson.getGender());
                             parentNode.add(newNode);
+                            int a = parentNode.getChildCount();
+                            System.out.println(a);
+                            tree.updateUI();
                             familyTree.put(personID,newNode);
-                            newFrame.pack();
                         }
-
+                        newFrame.pack();
+                    }
+                });
+                //Add spouse button
+                btn2.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Person newPerson = new Person();
+                        int spouseID = Integer.parseInt(getSpouseID.getText());
+                        String name = getNameSpouse.getText();
+                        String birthdate = getbirthDateSpouse.getText();
+                        String gender = getGenderSpouse.getText();
+                        if(spouseID == 0)
+                        {
+                            newPerson.setID(personID);
+                            newPerson.setName(name);
+                            newPerson.setBirthDate(birthdate);
+                            newPerson.setGender(gender);
+                            DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(newPerson.getName() + " - " + newPerson.getID() + " - " + newPerson.getBirthDate() + " - " + newPerson.getGender());
+                            tree = new JTree(rootNode);
+                            familyTree.put(personID,rootNode);
+                            leftPanel.add(tree);
+                        }
+                        else
+                        {
+                            newPerson.setID(++personID);
+                            newPerson.setName(name);
+                            newPerson.setGender(gender);
+                            newPerson.setBirthDate(birthdate);
+                            DefaultMutableTreeNode newTreeRootNode = new DefaultMutableTreeNode(newPerson.getName() + " - " + newPerson.getID() + " - " + newPerson.getBirthDate() + " - " + newPerson.getGender());
+                            familyTree.put(personID, newTreeRootNode);
+                            JTree newTree = new JTree(newTreeRootNode);;
+                            leftPanel.add(newTree);
+                        }
+                        newFrame.pack();
                     }
                 });
                 newFrame.add(mainSplitPane);
